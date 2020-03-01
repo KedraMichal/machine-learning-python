3f4267b051e58f7cc4c4abcb8492fc12d0a37567
ked-m <ked-mic@wp.pl>
2020-03-02 00:28:44 +0100

    logistic regression

--- logisticRegression.py
+++ logisticRegression.py
@@ -4,7 +4,7 @@ import sklearn
 from sklearn import model_selection
 from sklearn import linear_model
 from sklearn import linear_model
-
+from sklearn.metrics import confusion_matrix
 
 data = pd.read_csv("heart_disease_dataset.csv")
 
@@ -21,5 +21,13 @@ x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y
 logistic_r = linear_model.LogisticRegression()
 logistic_r.fit(x_train, y_train)
 
+predicted = logistic_r.predict(x_test)
+
+actual_vs_predicted = [(predicted[i], y_test[i]) for i in range(len(predicted))]
+print(actual_vs_predicted)
+
 acc = logistic_r.score(x_test, y_test)
 print(acc)
+
+conf = confusion_matrix(y_test, predicted)
+print(conf)
