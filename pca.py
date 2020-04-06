c322e36bfd5d85a53b60d0ab93180454113c4cb3
ked-m <ked-mic@wp.pl>
2020-04-06 21:30:45 +0200

    PCA

--- pca.py
+++ pca.py
@@ -2,25 +2,48 @@ import pandas as pd
 import sklearn
 from sklearn import decomposition, preprocessing
 import numpy as np
-np.set_printoptions(suppress=True)
+from sklearn.pipeline import Pipeline
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import GridSearchCV
+import matplotlib.pyplot as plt
 
+
+np.set_printoptions(suppress=True)
 df = pd.read_csv("cars.csv")
 
-df = df[["mpg", "cylinders", "displacement", "horsepower", "weight", "acceleration","year"]]
+df = df[["origin","mpg", "cylinders", "displacement", "horsepower", "weight", "acceleration","year"]]
 
 data = df.to_numpy()
-x = data[:,1:]
-y = data[:,0]
+x = data[:, 1:]
+y = data[:, 0]
 
-scaler = preprocessing.StandardScaler()
+# scaler = preprocessing.StandardScaler()
+# data = scaler.fit_transform(x)
+#
+# pca = decomposition.PCA(n_components=4)
+# pca1 = pca.fit_transform(data)
+#
+# print(pca1)
+# print(pca.explained_variance_ratio_)
 
-data = scaler.fit_transform(df)
 
-print(data)
-pca = decomposition.PCA(n_components=4)
-
-pca1 = pca.fit_transform(data)
-print(pca1)
+pipe = Pipeline([('scaler', preprocessing.StandardScaler()),
+                 ('reducer', decomposition.PCA(n_components=4)),
+                 ('knn', KNeighborsClassifier())])
 
 
-print(pca.explained_variance_ratio_)
+pipe.fit(x, y)
+print(pipe.steps[1][1].explained_variance_ratio_)
+
+plt.plot(pipe.steps[1][1].explained_variance_ratio_)
+
+plt.xlabel('Principal component index')
+plt.ylabel('Explained variance ratio')
+plt.show()
+
+parameters = {'knn__n_neighbors':[x for x in range(1, 20)]}
+
+grid = GridSearchCV(pipe, param_grid=parameters, cv=5)
+grid.fit(x, y)
+
+print(grid.best_params_)
